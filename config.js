15e41edc97033f706bdcb1108d4e9dcce4f11cde
wangbabe <106757091+wangbabe@users.noreply.github.com>
2022-06-02 22:07:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Gửi cho cậu <3",
     introTitle: 'Lê Hoàng Thảo Trang',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Cậu ơi, mình có một bí mật muốn nói cho cậu biết á, lại đây đi, tớ nói nhỏ thôi.`,
     btnIntro: 'Bấm chỗ này nè cậu :3',
-    title: 'Bởi vì cậu quá đỗi xinh đẹp, quá đỗi yêu kiều.',
+    title: 'Thật ra tớ thấy cậu rất là xinh đẹp luônn.',
     desc: 'Nên tớ lỡ thích cậu mất òi, phải làm sao phải làm sao, cậu thấy vậy có được không? ',
     btnYes: 'Được nha, tớ cũng thích cậu đóooo',
     btnNo: 'Không nha :3',
     question: 'Tớ thật lòng thích cậu lắm, cho tớ một cơ hội, nhé? 🥺',
-    btnReply: 'Gửi cho cậu <3',
-    reply: 'Oke nè',
-    mess: 'Yêu cậu lắm ấy, tối nay đi chơi với tớ nha?',
-    messDesc: 'Tối nay 7h, tớ qua đón đi chơi nha.',
+    btnYes: 'Oke nè',
+    btnNO: 'Không nha',
+    mess: 'Nếu cậu không chê thì tối nay đi chơi với tớ nha?',
+    messDesc: '7h nha babe?',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
